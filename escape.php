e492bae5a03560cdec4d8c02359e6497c5cf4bef
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-07-01 14:00:36 +0800

    added slicing and escape.php

---
+++ escape.php
@@ -0,0 +1,7 @@
+<?php 
+
+// This is a variable or storing string
+// Using escape characters to include quotes in a string
+$x = "We are the so-called \'Vikings\' from the north.";
+echo $x;
+?>
---
+++ slicing.php
@@ -0,0 +1,9 @@
+<?php 
+
+// This is a variable or storing string
+$x = "Hello World!";
+
+// Using substr to extract a part of the string
+// The first parameter is the string, the second is the starting position, and the third is the length of the substring
+echo substr($x, 6, 5);
+?>
