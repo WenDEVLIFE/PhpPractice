f6739e36f99e691ec5e4fe94bb275285c0a91f4d
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-07-01 13:55:34 +0800

    added a concate.php

---
+++ concate.php
@@ -0,0 +1,16 @@
+ <?php  
+
+ // This is a variable or storing string
+ $x = "Hello";
+$y = "World";
+$z = $x . " " . $y;
+echo $z;
+ 
+
+/// Using double quotes for string concatenation
+$x = "Hello";
+$y = "World";
+$z = "$x $y";
+echo $z;
+
+ ?>
--- index.php
+++ index.php
@@ -4,8 +4,36 @@
 
 <h1>My first PHP page</h1>
 <?php
+
+// This is a varialbe or storing string
 $txt = "你好 我是 Frouen!";
-echo "$txt";
+
+// integer variables
+$x= 5;
+$y= 5;
+
+echo $x + $y;
+echo $txt;
+
+// var_dump is used to display structured information (type and value)
+var_dump($x);
+
+
+// the method below is used to display the type of variable
+function myTest() {
+  $x = 5; // local scope
+  echo "<p>Variable x inside function is: $x</p>";
+}
+myTest();
+
+$x = "Hello World!";
+echo strtoupper($x);
+
+$x = "Hello World!";
+echo str_replace("World", "Dolly", $x);
+
+// using x outside the function will generate an error
+echo "<p>Variable x outside function is: $x</p>";
 ?>
 </body>
 </html>
