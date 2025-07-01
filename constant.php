b571bda8476b65fffa670fd9bb72e86178f70a90
WenDEVLIFE <frouenmedinajr@gmail.com>
2025-07-01 14:21:09 +0800

    added constant and math.php

---
+++ constant.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+define("GREETING", "Welcome to W3Schools.com!");
+echo GREETING;
+
+define("cars", [
+  "Alfa Romeo",
+  "BMW",
+  "Toyota"
+]);
+echo cars[0];
+
+function myTest() {
+  echo GREETING;
+}
+
+myTest();
+ echo GREETING;
+?>
---
+++ math.php
@@ -0,0 +1,19 @@
+<?php 
+
+echo(pi());
+
+echo(min(0, 150, 30, 20, -8, -200));
+echo(max(0, 150, 30, 20, -8, -200));
+
+
+echo(abs(-6.7));
+
+echo(sqrt(64));
+
+echo(round(0.60));
+echo(round(0.49));
+
+echo(rand());
+
+echo("random numbers generated \n" . rand(10, 100));
+?>
